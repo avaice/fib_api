fe53bf33cef1791950ccdf61589d88b5a09888e7
avaice <dev@cho-ice.xyz>
2023-10-23 16:20:55 +0900

    fix: use matrix when calculate fibonacci

--- src/logics/fibonacci/index.ts
+++ src/logics/fibonacci/index.ts
@@ -1,21 +1,12 @@
-const big = [...[0, 1, 2].map((v) => BigInt(v))]
-const memo = new Map<bigint, bigint>()
+import { Matrix } from '../matrix/index'
 
 export const fibonacci = (n: bigint): bigint => {
-  if (Number(n) < 0) {
-    throw new Error(
-      'The argument n for the fibonacci(n) must be a positive number.'
-    )
-  }
-  const memoValue = memo.get(n)
-  if (memoValue) {
-    return memoValue
-  }
-
-  if (n === big[0] || n === big[1]) {
-    return n
-  }
-  const result = fibonacci(n - big[1]) + fibonacci(n - big[2])
-  memo.set(n, result)
-  return result
+  const result = Matrix.pow(
+    [
+      [1n, 1n],
+      [1n, 0n],
+    ],
+    n - 1n
+  )
+  return result[0][0]
 }
---
+++ src/logics/matrix/index.ts
@@ -0,0 +1,30 @@
+type Matrix2 = [bigint, bigint]
+type Matrix2x2 = [Matrix2, Matrix2] // 2x2の行列型
+
+// 行列計算関数
+export const Matrix = {
+  mul: (a: Matrix2x2, b: Matrix2x2): Matrix2x2 => {
+    return [
+      [
+        a[0][0] * b[0][0] + a[0][1] * b[1][0],
+        a[0][0] * b[0][1] + a[0][1] * b[1][1],
+      ],
+      [
+        a[1][0] * b[0][0] + a[1][1] * b[1][0],
+        a[1][0] * b[0][1] + a[1][1] * b[1][1],
+      ],
+    ]
+  },
+  pow: (mat: Matrix2x2, n: bigint): Matrix2x2 => {
+    if (n === 1n) {
+      return mat
+    }
+    if (n % 2n === 0n) {
+      const half = Matrix.pow(mat, n / 2n)
+      return Matrix.mul(half, half)
+    } else {
+      const half = Matrix.pow(mat, (n - 1n) / 2n)
+      return Matrix.mul(Matrix.mul(half, half), mat)
+    }
+  },
+} as const
